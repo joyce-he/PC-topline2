30446f28a7ece5febccb80d0623bc7de2632378c
1122 <913009030@qq.com>
2019-10-08 11:35:16 +0800

    完成登录数据的验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,12 +7,12 @@
                 <img src="./logo_index.png" alt="">
             </div>
             <!-- el-form: 管理所有表单元素的父容器  ref: vue 中提供的操作 dom 的方式 model: 表单元素的数据源 label-width： 描述文本的宽度 -->
-            <el-form ref="form" :model="form">
+            <el-form ref="form" :model="form" :rules="rules">
                 <!-- el-form-item：表单域 label: 当前选项的描述文字 -->
-                <el-form-item>
+                <el-form-item prop="mobile">
                     <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
                 </el-form-item>
-                <el-form-item>
+                <el-form-item prop="code">
                     <!-- 一行 -->
                     <el-row>
                         <el-col :span="14">
@@ -24,7 +24,7 @@
                     </el-row>
                 </el-form-item>
                 <el-form-item>
-                    <el-button class="loginbtn" type="primary">登录</el-button>
+                    <el-button class="loginbtn" @click="login" type="primary">登录</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -38,7 +38,33 @@ export default {
       form: {
         mobile: '',
         code: ''
+      },
+      // 定义规则
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { min: 11, max: 11, message: '长度必须为11', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { min: 6, max: 6, message: '长度必须为6', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      // 得到 el-form 元素
+      // validate: 验证当前表单元素中所有的规则
+      this.$refs['form'].validate(valid => {
+        // 如果 valid 为 true 说明验证通过
+        // 如果 valid 为 false 说明验证不通过
+        if (valid) {
+          console.log('验证通过')
+        } else {
+          // 结束当前方法
+        }
+      })
     }
   }
 }
