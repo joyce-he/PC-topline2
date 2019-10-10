b2eb55ed0e9dab42ffb5707dc22e430c39d8cc7f
1122 <913009030@qq.com>
2019-10-10 16:35:39 +0800

    使用导航守卫完成登录验证

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ import Publish from '@/views/publish'
 Vue.use(Router)
 
 // 创建并且导出一个 Router 对象
-export default new Router({
+let router = new Router({
   // 设置路由选项
   routes: [
     // 根据目录的重定向
@@ -51,3 +51,29 @@ export default new Router({
     }
   ]
 })
+
+// router: 路由对象
+// 给路由对象添加导航守卫：全局前置导航守卫
+// 当由一个路由跳转到另一个路由时执行
+//  to: 要到达目标路由
+//  from: 发起跳转的路由
+//  next: 函数，是否执行后续代码
+router.beforeEach((to, from, next) => {
+  // 说明请求的路由发生了改变
+  // 排除跳转到登录页面
+  if (to.path !== '/login') {
+    // 得到 localstorage 中的 userInfo
+    let userInfo = window.localStorage.getItem('userInfo')
+    // 判断用户是否登录：
+    if (!userInfo) {
+      // 如果不存在说明没有登录过，应该跳转到登录页面
+      router.push('/login')
+    } else {
+      next()// 执行后续代码
+    }
+  } else {
+    next() // 执行后续代码
+  }
+})
+
+export default router
